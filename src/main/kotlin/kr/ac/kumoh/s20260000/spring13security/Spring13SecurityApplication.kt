5b13a1a712e71e98bac862af27f7b8e3141b1c04
prog4th <prog4th@proton.me>
2026-01-22 09:37:37 +0900

    🗃️MongoDB 연결

---
+++ src/main/kotlin/kr/ac/kumoh/s20260000/spring13security/Spring13SecurityApplication.kt
@@ -0,0 +1,11 @@
+package kr.ac.kumoh.s20260000.spring13security
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Spring13SecurityApplication
+
+fun main(args: Array<String>) {
+    runApplication<Spring13SecurityApplication>(*args)
+}
---
+++ src/test/kotlin/kr/ac/kumoh/s20260000/spring13security/Spring13SecurityApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.ac.kumoh.s20260000.spring13security
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Spring13SecurityApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
